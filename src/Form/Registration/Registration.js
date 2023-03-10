7c9f459f941dec0226afd54bcd9aa9b4258b9ecb
Dmytro Prykhodko <dmytro.prykhodko99@protonmail.com>
2023-03-10 12:13:41 +0200

    The password input switch was redesigned to use a React hook

--- src/Form/Registration/Registration.js
+++ src/Form/Registration/Registration.js
@@ -1,35 +1,41 @@
 import './Registration.css';
+import React from 'react';
+// function TypeInputSwitch()
+// {
+// const INPUT_ATTR = document.querySelector('.password-input__item');
+// const SWITCH = document.querySelector('.password-input__switch');
+// let type = INPUT_ATTR.getAttribute('type')
 
-function TypeInputSwitcher()
-{
-const INPUT_ATTR = document.querySelector('.password-input__item');
-const SWITCHER = document.querySelector('.password-input__switcher');
-let type = INPUT_ATTR.getAttribute('type')
-
-if(type === 'password'){
-  INPUT_ATTR.setAttribute('type', 'text')
-  SWITCHER.setAttribute('aria-checked', 'true')
-  SWITCHER.classList.add('password-input__switcher--active')
-}
-else{
-  INPUT_ATTR.setAttribute('type', 'password')
-  SWITCHER.setAttribute('aria-checked', 'false')
-  SWITCHER.classList.remove('password-input__switcher--active')
-}
-};
+// if(type === 'password'){
+//   INPUT_ATTR.setAttribute('type', 'text')
+//   SWITCH.setAttribute('aria-checked', 'true')
+//   SWITCH.classList.add('password-input__switch--active')
+// }
+// else{
+//   INPUT_ATTR.setAttribute('type', 'password')
+//   SWITCH.setAttribute('aria-checked', 'false')
+//   SWITCH.classList.remove('password-input__switch--active')
+// }
+// };
 
 
 let Registration = () =>{
+  let [type,setType] = React.useState('password');
+  const [activeClass, setClassName] = React.useState('');
+     let TypeInputSwitch = () => {
+      setType(type === "password" ? "text" : "password") 
+      setClassName(activeClass === '' ? "password-input__switch--active" : '')
+    }
   return (
     <div className="primary-form__register register-view">
       <form autoComplete="on" className="register-view__action">
         <fieldset>
         <legend aria-hidden="false">Форма реєстрації</legend>
         <h1>Реєстрації</h1>
-        <input type="email" className="rrr" placeholder="Ведіть почту..." aria-label="Ведіть почту" tab="0"/>
+        <input type="email" placeholder="Ведіть почту..." aria-label="Ведіть почту" tab="0"/>
         <div className="register-view__password-input password-input" >
-        <input type="password" className="password-input__item" placeholder="Ведіть пароль..." aria-label="Ведіть пароль" tab="1"/>
-        <button type="button" className="password-input__switcher"  onClick={TypeInputSwitcher} role="radio" aria-checked="false"></button>
+        <input type={type} className="password-input__item" placeholder="Ведіть пароль..." aria-label="Ведіть пароль" tab="1"/>
+        <button type="button" className={`password-input__switch ${activeClass}`}  onClick={TypeInputSwitch}></button>
         </div>
         <button type="submit">Зареєструватися</button>
         </fieldset>
