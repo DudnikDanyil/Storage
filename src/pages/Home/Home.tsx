5d0e10d39d2099bcb7e0ed9534b98747347f1f47
Dmytro Prykhodko <dmytro.prykhodko99@protonmail.com>
2023-03-21 18:35:55 +0200

    download button styled, accessibility added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Search from '../../components/Search/Search';
 import LogOutButton from '../../components/LogOutButton/LogOutButton';
+import UploadButton from '../../components/UploadButton/UploadButton'
 import './Home.scss'
 
 let Home = () =>{
   return (
     <header className='header'>
+         <UploadButton/>
          <Search/>
          <LogOutButton/>
     </header>
