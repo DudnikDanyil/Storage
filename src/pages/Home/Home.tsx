a2a4e6b710d0ea52383283d9d1b895d7d799341d
Dmytro Prykhodko <dmytro.prykhodko99@protonmail.com>
2023-03-21 16:45:35 +0200

    change Home.tsx,Home.scss

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Search from '../../components/Search/Search';
 import LogOutButton from '../../components/LogOutButton/LogOutButton';
+import './Home.scss'
 
 let Home = () =>{
   return (
-     <LogOutButton/>
+    <header className='header'>
+         <Search/>
+         <LogOutButton/>
+    </header>
   );
 }
 
